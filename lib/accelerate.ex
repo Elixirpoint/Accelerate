bfe9c04d79351f3fab1c97b36cae4a5312ccbfb1
CyberFlame <cyberflameu@gmail.com>
2021-06-28 11:28:18 +1200

    Various Elixir files
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>

---
+++ lib/accelerate.ex
@@ -0,0 +1,18 @@
+defmodule Accelerate do
+  @moduledoc """
+  Documentation for `Accelerate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Accelerate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
