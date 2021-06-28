8e07728d37c706c1988f513b94ef7ff24c5afa0b
CyberFlame <cyberflameu@gmail.com>
2021-06-28 18:13:39 +1200

    more of a start

--- accelerate/java/hibernate/Main.java
+++ accelerate/java/hibernate/Main.java
@@ -1,13 +1,11 @@
 import org.hibernate.HibernateException;
 import org.hibernate.Metamodel;
-import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import javax.persistence.metamodel.EntityType;
-
-import java.util.Map;
 
 public class Main
 {
---
+++ accelerate/java/src/main/java/net/elixirpoint/accelerate/Accelerate.java
@@ -0,0 +1,9 @@
+package net.elixirpoint.accelerate;
+
+public class Accelerate
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!");
+    }
+}
